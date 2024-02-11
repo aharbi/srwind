5535efc5601623a54fc9f28dd5e4ef9055988710
aalharbi <abdulazizaaharbi@gmail.com>
2024-02-11 14:47:32 -0800

    added dataset source

---
+++ dataset.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+mkdir dataset
+
+wget -P "dataset/" https://data.caltech.edu/records/czs3p-5ss80/files/train.zip
+wget -P "dataset/" "https://data.caltech.edu/records/czs3p-5ss80/files/val.zip"
+wget -P "dataset/" "https://data.caltech.edu/records/czs3p-5ss80/files/test.zip"
+
+unzip -q "dataset/train.zip" -d "dataset/"
+unzip -q "dataset/val.zip" -d "dataset/"
+unzip -q "dataset/test.zip" -d "dataset/"
