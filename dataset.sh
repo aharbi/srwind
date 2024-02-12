cb2051ac0a1f1ab4b030a256b6b00660f6d9d56e
aalharbi <abdulazizaaharbi@gmail.com>
2024-02-11 21:49:19 -0800

    changed unzipping package

--- dataset.sh
+++ dataset.sh
@@ -5,6 +5,11 @@ wget -P "dataset/" "https://data.caltech.edu/records/czs3p-5ss80/files/train.zip
 wget -P "dataset/" "https://data.caltech.edu/records/czs3p-5ss80/files/val.zip"
 wget -P "dataset/" "https://data.caltech.edu/records/czs3p-5ss80/files/test.zip"
 
-unzip -q "dataset/train.zip" -d "dataset/"
-unzip -q "dataset/val.zip" -d "dataset/"
-unzip -q "dataset/test.zip" -d "dataset/"
+# Uncomment to use unzip instead of 7z (However, train.zip seems to corrupted)
+#unzip -q "dataset/train.zip" -d "dataset/"
+#unzip -q "dataset/val.zip" -d "dataset/"
+#unzip -q "dataset/test.zip" -d "dataset/"
+
+7z x "dataset/train.zip"
+7z x "dataset/val.zip"
+7z x "dataset/test.zip"
