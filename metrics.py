f0de020241859db4257cd6f00c5bf07cb317f3a9
aalharbi <abdulazizaaharbi@gmail.com>
2024-02-12 10:19:57 -0800

    added metrics computation

--- metrics.py
+++ metrics.py
@@ -13,7 +13,8 @@ def peak_signal_to_noise_ratio(ref_patch: np.ndarray, HR_patch: np.ndarray):
         float: The peak signal-to-noise ratio (in dB)
     """
     rmse = 1 / 100 * np.linalg.norm(ref_patch.flatten() - HR_patch.flatten(), 2)
-    return 20 * np.log10(np.max(ref_patch) / rmse)
+    psnr = 20 * np.log10(np.max(ref_patch) / rmse)
+    return 100 if (np.isnan(psnr) or np.isinf(psnr)) else psnr
 
 
 def mean_squared_error(ref_patch: np.ndarray, HR_patch: np.ndarray):
@@ -64,3 +65,47 @@ def structural_similarity_index(ref_patch: np.ndarray, HR_patch: np.ndarray):
 
 def kinetic_energy_spectra(patch: np.ndarray):
     return None
+
+
+def compute_metrics(ref_patches: np.ndarray, HR_patches: np.ndarray):
+    """Computes the performance metrics given the reference HR patches and
+    the predicted HR patches.
+
+    Args:
+        ref_patches (np.ndarray): Array of size (batch_size, 2, 100, 100)
+        HR_patches (np.ndarray): Array of size (batch_size, 2, 100, 100)
+
+    Returns:
+        tuple: computed metrics as a tuple (PSNR, MSE, MAE, SSIM)
+    """
+
+    psnr_log = []
+    mse_log = []
+    mae_log = []
+    ssim_log = []
+
+    n = ref_patches.shape[0]
+    for channel_idx in [0, 1]:
+        for i in range(n):
+            psnr_log.append(
+                peak_signal_to_noise_ratio(
+                    ref_patches[i, channel_idx, :, :], HR_patches[i, channel_idx, :, :]
+                )
+            )
+            mse_log.append(
+                mean_squared_error(
+                    ref_patches[i, channel_idx, :, :], HR_patches[i, channel_idx, :, :]
+                )
+            )
+            mae_log.append(
+                mean_absolute_error(
+                    ref_patches[i, channel_idx, :, :], HR_patches[i, channel_idx, :, :]
+                )
+            )
+
+    avg_psnr = np.mean(psnr_log)
+    avg_mse = np.mean(mse_log)
+    avg_mae = np.mean(mae_log)
+    avg_ssim = 0
+
+    return avg_psnr, avg_mse, avg_mae, avg_ssim
