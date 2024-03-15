normalization fixes

kinetic_energy_norm.py
@@ -74,7 +74,7 @@ def kinetic_energy_spectra(
             # wind_profile_normalized = ((wind_profile - wind_profile.min()) / (wind_profile.max() - wind_profile.min()) * 255).astype(np.uint8)
             # wind_profile_normalized = wind_profile
             # Alternative: Normalization against the HR image
-            wind_profile_normalized = ((wind_profile_normalized - min) / (max - min) * 255).astype(np.uint8)
+            wind_profile_normalized = ((wind_profile - min) / (max - min) * 255).astype(np.uint8)
             image = Image.fromarray(wind_profile_normalized)
             HR_kvals2, HR_ek = energy_spectrum(image)
             Energy_Spectrum['HR']['x'].append(HR_kvals2)
@@ -85,7 +85,7 @@ def kinetic_energy_spectra(
             # wind_profile_normalized = ((wind_profile - wind_profile.min()) / (wind_profile.max() - wind_profile.min()) * 255).astype(np.uint8)
             # wind_profile_normalized = wind_profile
             # Alternative: Normalization against the HR image
-            wind_profile_normalized = ((wind_profile_normalized - min) / (max - min) * 255).astype(np.uint8)
+            wind_profile_normalized = ((wind_profile - min) / (max - min) * 255).astype(np.uint8)
             image = Image.fromarray(wind_profile_normalized)
             HR_kvals2, HR_ek = energy_spectrum(image)
             Energy_Spectrum['LR']['x'].append(HR_kvals2)